a58ce02c4508d200277714959e18ae71c387fb7c
HassanKrayem <neotion@outlook.com>
2017-03-10 16:52:11 +0200

    Create setting.php

---
+++ setting.php
@@ -0,0 +1,16 @@
+<?php
+
+
+  //setting page
+  
+  $a
+  [
+    'username' => 'Avan',
+    'email' 'avan@windows.com',
+    'hp' => 10,
+    'mp' => 30,
+    'Volume' => 10,
+    'BGM' => 'morning_BGM',
+    'Stage' => 3,
+  
+  ]
